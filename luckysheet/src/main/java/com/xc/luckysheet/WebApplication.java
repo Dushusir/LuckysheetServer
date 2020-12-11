1968e809ea3457e4a5437882ae12643ad1354052
631789194 <631789194@qq.com>
2020-12-11 15:16:40 +0800

    启动时，spring boot 不连接mongo数据库启动

--- luckysheet/src/main/java/com/xc/luckysheet/WebApplication.java
+++ luckysheet/src/main/java/com/xc/luckysheet/WebApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +18,8 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Slf4j
 @EnableScheduling
 @Configuration
-@SpringBootApplication(scanBasePackages = "com.xc")
+//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages = "com.xc",exclude={MongoAutoConfiguration.class})
 public class WebApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
